0613e5811503b36c9ef53f4451e5b6ecda38aaae
Christoph <chris.schroeder254@gmail.com>
2025-08-12 22:28:09 +0200

    reference netlif variable

--- netlify/functions/config.js
+++ netlify/functions/config.js
@@ -2,11 +2,7 @@
 exports.handler = async function(event, context) {
   return {
     statusCode: 200,
-    body: JSON.stringify({
-      // Example: Replace with your actual config keys
-      // apiKey: process.env.API_KEY,
-      // anotherSecret: process.env.ANOTHER_SECRET
-    }),
+    body: process.env.WEDDING_CONFIG,
     headers: {
       'Content-Type': 'application/json',
     },
